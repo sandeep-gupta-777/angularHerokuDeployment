bd32986a56c1a8cb429ad45d9e74dd847d0c149f
sandgup3 <sandeepkgupta007@gmail.com>
2017-11-22 21:01:54 +0530

    removed for SSL

--- server.js
+++ server.js
@@ -8,17 +8,18 @@ const app = express();
 // same url but with HTTPS
 const forceSSL = function() {
   return function (req, res, next) {
+    console.log('hello');
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
     next();
   }
-}
+};
 //checking
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+// app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
@@ -27,9 +28,12 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
+  console.log("cisco");
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function () {
+  console.log('listening');
+});

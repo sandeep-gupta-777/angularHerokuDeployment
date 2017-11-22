23fc73e78b45a8f6fcf24cf7de94ebd29ca2f044
sandgup3 <sandeepkgupta007@gmail.com>
2017-11-22 21:13:44 +0530

    removed for SSL

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080, function () {
+app.listen(process.env.PORT || 8090, function () {
   console.log('listening');
 });
